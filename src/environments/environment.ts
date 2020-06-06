23965f251f0443e698bb6cefd029394d93397a27
sadeemcapella <sadeem@capella.io>
2020-06-07 02:25:26 +0300

    proxy and endpoint fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  APIEndpoint: 'http://shoofny.site',
+  APIEndpoint: 'http://shoofny.site/en',
   production: false
 };
 
