ab6a74752198e99b2a524d1d01cf2e97a6355fb3
Sadeem Khalaf <sadeem@ahmads-MacBook-Pro.local>
2020-06-08 15:11:54 +0300

    reactiveforms for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -13,6 +13,7 @@ import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { Toast } from '@ionic-native/toast/ngx';
 import { CoreModule } from './core/core.module';
 import { HTTP } from '@ionic-native/http/ngx';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
@@ -21,6 +22,9 @@ import { HTTP } from '@ionic-native/http/ngx';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     CoreModule],
   providers: [
     StatusBar,
--- src/app/main/user/login/login.page.ts
+++ src/app/main/user/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppHelpersService } from 'src/app/core/utils/app-helpers.service';
 import { AuthService } from 'src/app/providers/auth.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +9,37 @@ import { AuthService } from 'src/app/providers/auth.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public form: FormGroup;
+  public isSubmitted = false;
 
   constructor(
+    public formBuilder: FormBuilder,
     public helper: AppHelpersService,
     private _auth: AuthService) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      password: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]]
+    });
+  }
+
+  get errorControl() {
+    return this.form.controls;
   }
 
   login(email, password) {
     this._auth.login(email, password);
+  }
+
+  onSubmitForm() {
+    this.isSubmitted = true;
+    if (this.form.valid) {
+      const email = this.form.get('email').value;
+      const password = this.form.get('password').value
+      this.login(email, password);
+      // this.login('admin@admin.com', 'admin');
+    }
   }
 
 }
