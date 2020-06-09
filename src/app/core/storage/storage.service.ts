38515b63cd43c4d1193704c3b223cab48c5f4fb3
Sadeem Khalaf <sadeem@ahmads-MacBook-Pro.local>
2020-06-09 04:32:43 +0300

    auth refresh token handled

--- src/app/core/storage/storage.service.ts
+++ src/app/core/storage/storage.service.ts
@@ -6,6 +6,7 @@ import { Token } from 'src/app/providers/auth.service';
 import { IUser } from 'src/app/models/user';
 
 const AUTH_STORAGE_KEY = 'AUTH_TOKEN';
+const AUTH_REFRESH_STORAGE_KEY = 'AUTH_REFRESH_TOKEN';
 const USER_DATA_STORAGE_KEY = 'userData';
 const API_STORAGE_KEY = 'storageKey';
 
@@ -19,7 +20,13 @@ export class StorageService implements IStorage {
   }
 
   public async updateUserToken(userData: Token) {
-    await this._storage.set(AUTH_STORAGE_KEY, userData.access).then(() => this._tokenSubject.next(userData.access));
+    await this._storage.set(AUTH_STORAGE_KEY, userData.access)
+    .then(() => this._tokenSubject.next(userData.access));
+    return userData;
+  }
+
+  public async updateRefreshToken(userData: Token) {
+    await this._storage.set(AUTH_REFRESH_STORAGE_KEY, userData.refresh);
     return userData;
   }
 
@@ -33,11 +40,18 @@ export class StorageService implements IStorage {
   }
 
   public async clearUserData() {
-    await this._storage.remove(AUTH_STORAGE_KEY).then(() => this._tokenSubject.next(undefined));
+    await this._storage.remove(AUTH_STORAGE_KEY)
+    .then(() => this._tokenSubject.next(undefined));
+    await this._storage.remove(AUTH_REFRESH_STORAGE_KEY);
+    await this._storage.remove(USER_DATA_STORAGE_KEY);
   }
 
   public async getAuthToken(): Promise<string> {
     return this._storage.get(AUTH_STORAGE_KEY);
+  }
+
+  public async getRefreshAuthToken(): Promise<string> {
+    return this._storage.get(AUTH_REFRESH_STORAGE_KEY);
   }
 
   // Save data
@@ -50,6 +64,7 @@ export class StorageService implements IStorage {
   public getLocalData<T = any>(key: string): Promise<T> {
     console.log('Getting cached data for: ', `${API_STORAGE_KEY}-${key}`);
     return this._storage.get(`${API_STORAGE_KEY}-${key}`)
-      .then(data => !!data ? Promise.resolve(JSON.parse(data)) : Promise.reject({message: 'No Cached data for Offline support found'}));
+      .then(data => !!data ? Promise.resolve(JSON.parse(data)) 
+      : Promise.reject({ message: 'No Cached data for Offline support found' }));
   }
 }
--- src/app/providers/auth.service.ts
+++ src/app/providers/auth.service.ts
@@ -4,9 +4,10 @@ import { IUser } from 'src/app/models/user';
 import { AuthHttp } from '../core/auth-http/auth-http.service';
 import { NetworkService, ConnectionStatus } from '../core/utils/network.service';
 import { StorageService } from '../core/storage/storage.service';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AppHelpersService } from '../core/utils/app-helpers.service';
+import { take } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 export class Token {
   access: string;
@@ -22,27 +23,36 @@ export class AuthService {
 
   constructor(
     private _http: AuthHttp,
+    private _httpClient: HttpClient,
     private _networkService: NetworkService,
     private _storageService: StorageService,
     private _helper: AppHelpersService,
     private _route: Router) {
-      this.loggedInUser = this._loggedInUser.asObservable();
-      this._checkLoggedIn();
-    }
+    this.loggedInUser = this._loggedInUser.asObservable();
+    this._checkLoggedIn();
+    this._storageService.getRefreshAuthToken().then((refresh) => {
+      this._refreshToken(refresh);
+    }, error => { 
+      console.warn('token missing!');
+      this._route.navigate(['/login']); });
+  }
 
   // login
   public login(e_mail: string, pass_word: string) {
-    this._getAccessToken({email: e_mail, password: pass_word});
+    this._getAccessToken({ email: e_mail, password: pass_word });
   }
 
   // TODO:
   // logout
+  public logout() {
+      return this._http.post('api/auth/logout/', {});
+  }
 
   private _getAccessToken(data) {
     this._helper.showLoading();
     return new Promise<Token>(
       (resolve, reject) =>
-          this._http.post('/api/token/', data)
+        this._http.post('/api/token/', data)
           .subscribe((token: any) => {
             this._helper.hideLoading();
             if (!!token) {
@@ -52,7 +62,7 @@ export class AuthService {
               });
             } else {
               this._helper.hideLoading();
-              reject({message: 'Wrong username and password'});
+              reject({ message: 'Wrong username and password' });
             }
           }, error => {
             this._helper.hideLoading();
@@ -65,9 +75,9 @@ export class AuthService {
       .then((response) => this._loggedInUser.next(userData), (reason) => console.warn(reason));
   }
 
-  private _setUserToken(userData: Token): Promise<any> {
-    return this._storageService.updateUserToken(userData)
-      .then((response) => {}, (reason) => console.warn(reason));
+  private async _setUserToken(userData: Token): Promise<any> {
+    await this._storageService.updateRefreshToken(userData);
+    await this._storageService.updateUserToken(userData);
   }
 
   private _removeUserData(): Promise<any> {
@@ -78,19 +88,30 @@ export class AuthService {
   private _fillUserData(resolve, reject) {
     this._http.get<IUser>('/api/auth/profile/')
       .subscribe((userData: IUser) => this._setUserData(userData)
-      .then(() => {
-        resolve(userData);
-        this._route.navigate(['/home']);
-      }), (reason) => {
-        if (reason.status >= 400 && reason.status < 500) {
-          this._removeUserData().then(() => {
-            console.warn('Token missing or expired');
+        .then(() => {
+          resolve(userData);
+          this._route.navigate(['/home']);
+        }), (reason) => {
+          if (reason.status >= 400 && reason.status < 500) {
+            if (reason.status == 401) {
+              // send refresh token
+              this._storageService.getRefreshAuthToken().then((refresh) => {
+                this._refreshToken(refresh);
+                console.log(refresh);
+              }, error => { 
+                console.warn('token missing!');
+                this._route.navigate(['/login']); })
+            } else {
+              this._removeUserData().then(() => {
+                this._route.navigate(['/login']);
+                console.warn('Token missing or expired');
+                reject(reason);
+              });
+            }
+          } else {
             reject(reason);
-          });
-        } else {
-          reject(reason);
-        }
-      });
+          }
+        });
   }
 
   private _checkLoggedIn(): Promise<IUser> {
@@ -98,14 +119,33 @@ export class AuthService {
       this._networkService.onNetworkChange().subscribe((status: ConnectionStatus) => {
         if (status === ConnectionStatus.Online) {
           this._storageService.getAuthToken()
-          .then((token) => this._fillUserData(resolve, reject),
-          reject => {
-            console.warn(reject);
-          })
+            .then(() => this._fillUserData(resolve, reject),
+              reject => {
+                console.warn(reject);
+              })
         } else {
           console.warn('You are offline!');
         }
       });
     });
   }
+
+  public _refreshToken(refreshToken: string) {
+    return new Promise<Token>(
+      (resolve, reject) =>
+        this._http.post('/api/token/refresh/', { refresh: refreshToken })
+          .subscribe((token: any) => {
+            if (!!token) {
+              console.log(token);
+              this._storageService.updateUserToken(token)
+                .then(() => resolve(token));
+            } else {
+              reject({ message: 'token missing or expired' });
+            }
+          }, error => {
+            // reject(error);
+            this._route.navigate(['/login']);
+          }));
+  }
+
 }
