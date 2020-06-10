82d458405554084d727abc7f5f7a508e0c0996e9
Sadeem Khalaf <sadeem@ahmads-MacBook-Pro.local>
2020-06-10 12:37:03 +0300

    home page changes

--- src/app/main/details/home/home.page.ts
+++ src/app/main/details/home/home.page.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { AuthService } from 'src/app/providers/auth.service';
-
+import { IUser } from 'src/app/models/user';
+import { StorageService } from 'src/app/core/storage/storage.service';
+import { take } from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { JobDetailsService } from 'src/app/providers/job-details.service';
+import { IJobDetails } from 'src/app/models/Job';
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
@@ -8,9 +13,39 @@ import { AuthService } from 'src/app/providers/auth.service';
 })
 export class HomePage implements OnInit {
 
-  constructor(private _auth: AuthService) { }
+  public profileImage: string;
+  public userData: IUser;
+  public jobsCountByCity: number = 0;
+  public jobsListByCity: IJobDetails[] = [];
+
+  constructor(
+    private _auth: AuthService,
+    private _route: Router,
+    private _jobDetailsService: JobDetailsService,
+    private _storage: StorageService) { }
 
   ngOnInit() {
+    this._storage.getUserData().then((data) => {
+      this.userData = data;
+      this.getJobsByCity(595)
+        .pipe(take(1))
+        .subscribe((data: any) => {
+          this.jobsCountByCity = data.count;
+          this.jobsListByCity = data.results as IJobDetails[];
+        });
+      if (!!this.userData.picture) {
+        document.getElementsByClassName('image-circle')
+          .item(0).setAttribute('style', `backgroundImage: url('${this.userData.picture}')`);
+      }
+    })
+  }
+
+  public logout() {
+    this._auth.logout();
+  }
+
+  public getJobsByCity(id: number) {
+    return this._jobDetailsService.getJobByParam({ city: id });
   }
 
 }
